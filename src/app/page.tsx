cec860a8f589a6c33c374e7cae347f904dca6f99
Philihp Busby <philihp@gmail.com>
2023-09-10 21:35:43 +0000

    prettier husky linting setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import styles from './page.module.css'
 
-const Home= () => 
-    <main className={styles.main}>
-      <div className={styles.center}>
-        Center
-      </div>
-    </main>
+    const Home= () => 
+        <main className={styles.main}>
+          <div className={styles.center}>
+            Center
+          </div>
+        </main>
     
 export default Home
